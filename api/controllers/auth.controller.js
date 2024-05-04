647ab1bd20ced34369bd6cb72ed24f1d662b8543
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-05-04 12:20:05 +0530

    create signup api routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+
+export const signup = async (req, res) => {
+    //console.log(req.body)
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return res.status(400).json({ massage: 'All fields are required' })
+    }
+
+    const hashedpassword = bcryptjs.hashSync(password,10)
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedpassword,
+    });
+
+   try {
+    await newUser.save();
+        res.json({ massage: 'signup successful' });
+    
+   } catch (error) {
+    res.status(500).json({massage:error.massage });
+   }
+
+
+    
+
+};
