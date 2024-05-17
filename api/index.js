07b0b43ee620c87a62660c8122fe6e426f3ae3df
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-05-17 11:01:10 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from'path';
 
 
 dotenv.config();
@@ -16,6 +17,8 @@ mongoose
 }).catch((err)=>{
     console.log(err);
 })
+
+const _dirname =path.resolve();
 
 const app = express();
 
@@ -31,7 +34,13 @@ app.listen(3000, ()=>{
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post', postRoutes);
-app.use('/api/comment',commentRoutes)
+app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(_dirname,'client', 'dist', 'index.html'))
+});
 
 app.use((err,req,res,next)=>{
     const statuscode = err.statuscode||500;
