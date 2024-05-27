85cfcca71163d6ccbc955cd7f9a4e4359e5ccefe
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-05-28 00:35:07 +0530

    edit some UI Changers

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector} from 'react-redux';
 import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
+import OAuth from '../component/OAuth';
 
 export default function SignIn() {
 
@@ -50,11 +51,11 @@ export default function SignIn() {
    };
   return (
     <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex p-1 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
 
 
         {/*left*/}
-        <div className='flex-1'></div>
+        <div className='flex-1'>
         <Link to="/" className='font-bold dark:text-white text-5xl'>
 
           <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
@@ -68,6 +69,7 @@ export default function SignIn() {
          or with Google.
 
         </p>
+        </div>
 
 
 
@@ -101,6 +103,8 @@ export default function SignIn() {
              }
              
              </Button>
+
+             <OAuth/>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>
